bba703978699e5f2536ceb0d2e2a93ad998a11bf
Omar S <osanseviero@gmail.com>
2016-11-15 11:17:25 -0600

    feat: The simulation now includes different arrival rates per hour and allows to simulate different configuration

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ This is a simulation of a bank with the next features
  - For rush hours, numbers will be Triangular-distributed random variables.
  - Time in the system is managed in hours. The rates of arrivals are different at every hour.
  - System also calculates which is the cost of mantaining the system, and is easily reusable to test with different configurations and probability tables.
+
+
+Server configurations
+	server1.json
+		2 servers for preferencial customers
+		1 server for normal customer
+		1 server for no customer
+	server2.json
+		2 servers for preferencial customers
+		1 server for no customer
+	server3.json
+		1 server for preferencial customer
+		1 server for normal customer
+		1 server for no customer
--- simulate.py
+++ simulate.py
@@ -1,7 +1,37 @@
 import arrivals
 import servers
 import probTable
+from operator import add
 
-arrivalDict =  probTable.generateDictOfProb('arrivals')
-customers = arrivals.simulateArrivals(arrivalDict)
-servers.servePeople(customers)
+arrivalFiles = ['arrivals/a830_9', 'arrivals/a9_930', 'arrivals/a930_10', 'arrivals/a10_1030', 'arrivals/a1030_11',
+					'arrivals/a11_1130', 'arrivals/a1130_12', 'arrivals/a12_1230', 'arrivals/a1230_13', 'arrivals/a13_1330',
+					'arrivals/a1330_1400', 'arrivals/a1400_1430', 'arrivals/a1430_1500', 'arrivals/a15_1530']
+
+timePeriods = ['8:30-9', '9-9:30', '9:30-10', '10-10:30', '10:30-11', '11-11:30', '11:30-12', '12-12:30', '12:30-13'
+				'13-13:30', '13:30-14', '14-14:30', '14:30-15', '15-15:30', '15:30-16', '16-16:30', '16:30-17']
+
+
+salaryCost = 0
+waitingCustomerCost = 0
+customers = [0, 0, 0]
+timeIdx = 0
+for idx, arrival in enumerate(arrivalFiles):
+	print "\nSimulating " + timePeriods[idx]
+	arrivalDict =  probTable.generateDictOfProb(arrival)
+	customers = map(add, customers, arrivals.simulateArrivals(arrivalDict))
+	customers = servers.servePeople(customers)
+	salaryCost = salaryCost + servers.calculateSalary()
+	timeIdx = idx + 1
+	for idx, customer in enumerate(customers):
+		waitingCustomerCost = waitingCustomerCost + customer * (idx + 2)
+
+while customers != [0, 0, 0]:
+	print "\nThere are still customers in the system. Working more..."
+	print "\nSimulating " + timePeriods[timeIdx]
+	timeIdx = timeIdx + 1
+	customers = servers.servePeople(customers)
+	salaryCost = salaryCost + servers.calculateSalary()
+
+print "Total cost of salaryCost: " , salaryCost
+print "Total cost of waiting customers: " , waitingCustomerCost
+print "Total cost: ", salaryCost + waitingCustomerCost
